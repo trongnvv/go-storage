5e8e2ed61e89e5fbb1fc5d6925da26864dd6bc4a
trongnvv <trongnv138@gmail.com>
2021-02-18 09:37:29 +0700

    upload file

--- controllers/storage_controller.go
+++ controllers/storage_controller.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"fmt"
 	"gostorage/models"
+	"io"
+	"os"
 
 	"net/http"
 
@@ -51,40 +53,46 @@ func (c StorageController) Register(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func registFile(name string, isError ...bool) (*os.File, error) {
+	path := "upload"
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		err := os.Mkdir(path, 0755)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	dst, err := os.Create("upload/" + name)
+	if err != nil {
+		return nil, err
+	}
+	return dst, nil
+}
+
 func (c StorageController) Upload(w http.ResponseWriter, r *http.Request) {
 	// Parse our multipart form, 10 << 20 specifies a maximum
 	// upload of 10 MB files.
 	r.ParseMultipartForm(10 << 20)
-	// FormFile returns the first file for the given key `myFile`
-	// it also returns the FileHeader so we can get the Filename,
-	// the Header and the size of the file
-	file, handler, err := r.FormFile("myFile")
+	file, handler, err := r.FormFile("file")
 	if err != nil {
 		fmt.Println("Error Retrieving the File")
 		c.respond(w, nil, http.StatusBadRequest, err.Error())
 		return
 	}
 	defer file.Close()
-	fmt.Printf("Uploaded File: %+v\n", handler.Filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	fmt.Printf("[Upload] Uploaded File: %+v\n", handler.Filename)
+	fmt.Printf("[Upload] File Size: %+v\n", handler.Size)
 
-	// Create a temporary file within our temp-images directory that follows
-	// a particular naming pattern
-	// tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// defer tempFile.Close()
+	dst, err := registFile(handler.Filename)
+	defer dst.Close()
+	if err != nil {
+		c.respond(w, nil, http.StatusBadRequest, err.Error())
+		return
+	}
+	if _, err := io.Copy(dst, file); err != nil {
+		c.respond(w, nil, http.StatusBadRequest, err.Error())
+		return
+	}
 
-	// // read all of the contents of our uploaded file into a
-	// // byte array
-	// fileBytes, err := ioutil.ReadAll(file)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// // write this byte array to our temporary file
-	// tempFile.Write(fileBytes)
-	// return that we have successfully uploaded our file!
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
+	c.respond(w, nil, http.StatusBadRequest, "Successfully Uploaded File")
 }
